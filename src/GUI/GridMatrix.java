2e98583e5b29ab642e411b160711313bf2109b14
Ahmed Hesham <96313662+AH-HESHAM@users.noreply.github.com>
2022-12-01 17:09:59 +0200

    modify max number of equations

--- src/GUI/GridMatrix.java
+++ src/GUI/GridMatrix.java
@@ -17,40 +17,46 @@ public class GridMatrix implements ActionListener {
     private double results[];
     private JButton GotoType;
     private int NumofEquations;
-    GridMatrix(JPanel Logic, int NumofEquations){
+
+    GridMatrix(JPanel Logic, int NumofEquations) {
         this.Logic = Logic;
         this.NumofEquations = NumofEquations;
         EquationsLayout();
     }
 
-    public void EquationsLayout(){
-        int startX = 150, startY = 100;
-        variablesName = new JLabel[ NumofEquations ][ NumofEquations ];
-        pocket = new JTextField[ NumofEquations ][ NumofEquations+1 ];
+    public void EquationsLayout() {
+        // int startX = (1400 - 120 * (NumofEquations + 1)) / 2, startY = 10 * (10 -
+        // NumofEquations + 1);
+        int startX = (1400 - 120 * (NumofEquations + 1)) / 2, startY = (10 + (700 - 70 * NumofEquations)) / 2;
+        variablesName = new JLabel[NumofEquations][NumofEquations];
+        pocket = new JTextField[NumofEquations][NumofEquations + 1];
 
         // distance added reversely relational with Number of equations
-        int distanceAdded = 50;
+        int distanceAdded = 60;
 
-        //labels
+        // labels
         for (int i = 0; i < NumofEquations; i++) {
             for (int j = 0; j < NumofEquations; j++) {
 
-                String s = (j != NumofEquations - 1)? String.format(" x%d + ", j+1) : String.format(" x%d = ", j+1);
+                String s = (j != NumofEquations - 1) ? String.format(" x%d + ", j + 1)
+                        : String.format(" x%d = ", j + 1);
                 variablesName[i][j] = new JLabel(s);
-                variablesName[i][j].setBounds(startX + (2*j+2)*distanceAdded, startY + i * distanceAdded, 50, distanceAdded);
-                variablesName[i][j].setFont(new Font( "Times New Roman", Font.PLAIN, 20 ));
+                variablesName[i][j].setBounds(startX + (2 * j + 2) * distanceAdded, startY + i * distanceAdded, 60,
+                        distanceAdded);
+                variablesName[i][j].setFont(new Font("Times New Roman", Font.PLAIN, 20));
                 variablesName[i][j].setHorizontalAlignment(JLabel.CENTER);
-                Logic.add( variablesName[i][j] );
+                Logic.add(variablesName[i][j]);
             }
         }
 
         for (int i = 0; i < NumofEquations; i++) {
             for (int j = 0; j <= NumofEquations; j++) {
                 pocket[i][j] = new JTextField();
-                pocket[i][j].setBounds(startX + j * distanceAdded + distanceAdded * (j+1), startY + i * distanceAdded, 50, distanceAdded - 20);
-                pocket[i][j].setFont(new Font( "Times New Roman", Font.PLAIN, 30 ));
+                pocket[i][j].setBounds(startX + j * distanceAdded + distanceAdded * (j + 1), startY + i * distanceAdded,
+                        50, distanceAdded - 20);
+                pocket[i][j].setFont(new Font("Times New Roman", Font.PLAIN, 30));
                 pocket[i][j].setHorizontalAlignment(JLabel.CENTER);
-                Logic.add( pocket[i][j] );
+                Logic.add(pocket[i][j]);
             }
         }
 
@@ -65,29 +71,28 @@ public class GridMatrix implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if( e.getSource() == GotoType ){
+        if (e.getSource() == GotoType) {
 
             // intialize the user arrays
-            cofficients = new double[ NumofEquations ][ NumofEquations ];
-            results = new double[ NumofEquations ];
+            cofficients = new double[NumofEquations][NumofEquations];
+            results = new double[NumofEquations];
 
             // store the values got from user and change visibility.
             for (int i = 0; i < NumofEquations; i++) {
                 for (int j = 0; j <= NumofEquations; j++) {
-                    if(j != NumofEquations){
+                    if (j != NumofEquations) {
                         cofficients[i][j] = parseDouble(pocket[i][j].getText());
                         variablesName[i][j].setVisible(false);
                         pocket[i][j].setVisible(false);
-                    }
-                    else{
+                    } else {
                         results[i] = parseDouble(pocket[i][j].getText());
                         pocket[i][j].setVisible(false);
                     }
                 }
             }
             GotoType.setVisible(false);
-            System.out.println( Arrays.toString(results) );
-            System.out.println( Arrays.deepToString(cofficients) );
+            System.out.println(Arrays.toString(results));
+            System.out.println(Arrays.deepToString(cofficients));
             new wayToSolve(Logic, cofficients, results);
         }
     }
