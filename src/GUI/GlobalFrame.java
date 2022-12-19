572371b911d93c123e39104628d5470b86662833
abdelrahman mohamed mohamed abdelmotlb <91913879+abdelmotlb@users.noreply.github.com>
2022-12-20 01:36:55 +0200

    fix wrong code jacobi

---
+++ src/GUI/GlobalFrame.java
@@ -0,0 +1,190 @@
+package GUI;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+
+public class GlobalFrame extends JFrame implements ActionListener {
+    public static int precision;
+    public static boolean useScaling = true; // GlobalFrame.useScaling
+    public static final Color usedColor = new Color(0xFFFFFF);
+    public static final Color secUsedColor = new Color(0x051F2A);
+    public static final Color background = new Color(0x063548);
+
+
+    private int NumofEquations;
+    private JPanel Logic;
+    private JLabel NumEqLabel;
+    private JTextField txtOfNum;
+    private JLabel precisionLabel;
+    private JTextField txtOfPrecision;
+    private JLabel scalingLabel;
+    private JButton scalingBut;
+    private JButton SubmitSize;
+    private JButton tempBut;
+
+    // constructor
+    public GlobalFrame() {
+
+
+
+
+        // photo of app
+        String IconLoc = "src/GUI/logo.png";
+        ImageIcon img = new ImageIcon(IconLoc);
+        this.setIconImage(img.getImage());
+
+        // headerLabel
+        JLabel header = new JLabel("System solver");
+        header.setBounds(0, 0, 1400, 100);
+        header.setHorizontalAlignment(JLabel.CENTER);
+        header.setVerticalAlignment(JLabel.CENTER);
+        header.setBackground(usedColor);
+        header.setForeground(new Color(0xFFFFFF));
+        header.setFont(new Font("Times New Roman", Font.PLAIN, 52));
+        header.setOpaque(true);
+
+        // logic part
+        Logic = new JPanel();
+        Logic.setBounds(0, 100, 1400, 800);
+        Logic.setBackground(background);
+        Logic.setOpaque(true);
+        Logic.setLayout(new BorderLayout());
+        Logic.setBorder(new EmptyBorder(800, 1400, 0, 0));
+        //        Logic.setLayout(null);
+
+
+//        // temp but
+//        JButton but = new JButton("temp");
+//        setBounds(1500, 900, 200, 100);
+
+        // get number of equations
+        NumEqLabel = new JLabel();
+        NumEqLabel.setBounds(0, 20, 1400, 100);
+        NumEqLabel.setText("        → Enter the number of variables");
+        NumEqLabel.setFont(new Font("Times New Roman", Font.ITALIC, 50));
+        NumEqLabel.setForeground(usedColor);
+
+        txtOfNum = new JTextField();
+        txtOfNum.setBounds(550, 150, 300, 70);
+        txtOfNum.setHorizontalAlignment(JTextField.CENTER);
+        txtOfNum.setFont(new Font("Times New Roman", Font.ITALIC, 50));
+
+        // get precision
+        precisionLabel = new JLabel();
+        precisionLabel.setBounds(0, 250, 1400, 100);
+        precisionLabel.setText("        → Enter precision used");
+        precisionLabel.setForeground(usedColor);
+        precisionLabel.setFont(new Font("Times New Roman", Font.ITALIC, 50));
+
+
+        txtOfPrecision = new JTextField();
+        txtOfPrecision.setBounds(550, 380, 300, 70);
+        txtOfPrecision.setHorizontalAlignment(JTextField.CENTER);
+        txtOfPrecision.setFont(new Font("Times New Roman", Font.ITALIC, 50));
+
+        // check scaling
+        scalingLabel = new JLabel();
+        scalingLabel.setBounds(0, 480, 1400, 100);
+        scalingLabel.setText("        → click to toggle");
+        scalingLabel.setFont(new Font("Times New Roman", Font.ITALIC, 50));
+        scalingLabel.setForeground(usedColor);
+
+        scalingBut = new JButton("use Scaling");
+        scalingBut.setBounds(550, 600, 300, 50);
+        scalingBut.setBackground(secUsedColor);
+        scalingBut.setForeground(new Color(0xFFFFFF));
+        scalingBut.setFont(new Font("Arial", Font.BOLD, 20));
+        scalingBut.addActionListener(this);
+
+
+        // submission button
+        SubmitSize = new JButton("GO!");
+        SubmitSize.setBounds(1150, 680, 200, 50);
+        SubmitSize.setBackground(secUsedColor);
+        SubmitSize.setForeground(usedColor);
+        SubmitSize.setFont(new Font("Arial", Font.BOLD, 20));
+        SubmitSize.addActionListener(this);
+
+        tempBut = new JButton();
+        tempBut.setBackground(background);
+        tempBut.setEnabled(false);
+        tempBut.setBorder(null);
+        tempBut.setBounds(1300, 700, 1, 1);
+
+        // frame settings
+        this.setTitle("Matrix solver");
+        this.setSize(1400, 800);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Logic.add(NumEqLabel);
+        Logic.add(txtOfNum);
+        Logic.add(precisionLabel);
+        Logic.add(txtOfPrecision);
+        Logic.add(scalingLabel);
+        Logic.add(scalingBut);
+        Logic.add(SubmitSize);
+        Logic.add(tempBut);
+
+
+
+//        // scroll:
+//        JScrollPane scrollPane = new JScrollPane(Logic, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+//        scrollPane.setPreferredSize(new Dimension(1200, 400));
+//        scrollPane.setVisible(true);
+//        Logic.add(scrollPane);
+//        this.add(S);
+
+//        this.add(Logic);
+        JScrollPane scroller = new JScrollPane(Logic);
+        this.add(BorderLayout.CENTER, scroller);
+        scroller.setWheelScrollingEnabled(true);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+//        this.add(header);
+        this.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == SubmitSize) {
+            try { NumofEquations = parseInt(txtOfNum.getText()); }
+            catch(NumberFormatException ea) { NumofEquations = 5; } // default NumOfEquations
+
+            try { precision = parseInt(txtOfPrecision.getText()); }
+            catch(NumberFormatException ea) { precision = 8; } // default precision
+
+            NumEqLabel.setVisible(false);
+            txtOfNum.setVisible(false);
+            precisionLabel.setVisible(false);
+            txtOfPrecision.setVisible(false);
+            SubmitSize.setVisible(false);
+            scalingLabel.setVisible(false);
+            scalingBut.setVisible(false);
+            tempBut.setVisible(false);
+
+
+            new GridMatrix(Logic, NumofEquations);
+
+            // System.out.println(NumofEquations);
+        }
+        else if( e.getSource() == scalingBut ){
+            if( useScaling ){
+                useScaling = false;
+                scalingBut.setText("No scaling");
+            }else {
+                useScaling = true;
+                scalingBut.setText("use scaling");
+            }
+        }
+    }
+
+}
---
+++ src/logic/chelosky.java
@@ -0,0 +1,178 @@
+package logic;
+
+import GUI.GlobalFrame;
+
+public class chelosky {
+
+    private boolean valid = true;
+    private double[][] arr2;
+    private int n;
+    private double[] b2;
+    private double[] y;
+    private double[] ans;
+    private long time;
+    private int pres = GlobalFrame.precision;
+
+    public chelosky(double[][] arr, double[] b) {
+        this.n = arr.length;
+        arr2 = new double[n][n];
+        b2 = new double[n];
+        // copy equations to keep original
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr2[i][j] = approximation.sigFig(arr[i][j], pres);
+            }
+            b2[i] = approximation.sigFig(b[i], pres);
+        }
+    }
+
+    private int scaling(int row) {
+        double[] temp = new double[n - row];
+        int maxIndex = row;
+        for (int i = row; i < n; i++) {
+            double mymax = arr2[i][row];
+            if (mymax <= 0)
+                mymax = -1;
+            for (int j = row + 1; j < n; j++) {
+                double toComp = arr2[i][j];
+                if (toComp < 0) {
+                    toComp = -1;
+                }
+                if (toComp > mymax) {
+                    mymax = toComp;
+                }
+            }
+            if (mymax == 0) {
+                valid = false;
+                break;
+            }
+
+            temp[i - row] = arr2[i][row] / mymax;
+            if (temp[i - row] < 0)
+                temp[i - row] *= -1;
+        }
+        for (int i = 0; i < n - row - 1; i++) {
+            if (temp[i + 1] > temp[i])
+                maxIndex = i + 1 + row;
+        }
+        return maxIndex;
+    }
+
+    private void pivoting(int row) {
+        int maxIndex;
+        if (GlobalFrame.useScaling)
+            maxIndex = scaling(row);
+        else {
+            maxIndex = row;
+            double maxn = Math.abs(arr2[row][row]);
+            for (int i = row + 1; i < n; i++) {
+                if (Math.abs(arr2[i][row]) > maxn) {
+                    maxn = arr2[i][row];
+                    maxIndex = i;
+                }
+            }
+        }
+        if (maxIndex >= 0) {
+            if (arr2[row][maxIndex] != 0) {
+                if (maxIndex != row) {
+                    double[] temp = new double[n];
+                    temp = arr2[row];
+                    arr2[row] = arr2[maxIndex];
+                    arr2[maxIndex] = temp;
+                    double temp2 = b2[row];
+                    b2[row] = b2[maxIndex];
+                    b2[maxIndex] = temp2;
+                }
+            } else
+                valid = false;
+        } else
+            valid = false;
+    }
+
+    private void forElimination() {
+        int n = arr2.length;
+        for (int k = 0; k < n - 1 && valid; k++) {
+            pivoting(k);
+            for (int i = k + 1; i < n; i++) {
+                if (arr2[k][k] == 0) {
+                    valid = false;
+                    return;
+                }
+                double factor = approximation.sigFig(arr2[i][k] / arr2[k][k], pres);
+                arr2[i][k] = factor;
+                for (int j = k + 1; j < n; j++)
+                    arr2[i][j] = approximation.sigFig(arr2[i][j] - factor * arr2[k][j], pres);
+            }
+        }
+        // New U
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                arr2[i][j] = approximation.sigFig(arr2[i][j] / arr2[i][i], pres);
+            }
+        }
+    }
+
+    // LY = B
+    // DUX = Y
+
+    public void forSubstitution() {
+        y[0] = b2[0];
+        for (int i = 1; i < n; i++) {
+            double sum = 0;
+            for (int j = 0; j < i; j++) {
+                sum = approximation.sigFig(sum + (y[j] * arr2[i][j]), pres);
+            }
+            y[i] = approximation.sigFig(b2[i] - sum, pres);
+        }
+    }
+
+    public void DiagonalEvaluation() {
+        for (int i = 0; i < n; i++) {
+            y[i] = approximation.sigFig(y[i] / arr2[i][i], pres);
+        }
+    }
+
+    // Next
+    // DUX = Y
+
+    private void backSubstitution() {
+        int n = arr2.length;
+        if (arr2[n - 1][n - 1] == 0) {
+            valid = false;
+            return;
+        }
+        ans[n - 1] = approximation.sigFig(y[n - 1], pres);
+        for (int i = n - 2; i >= 0; i--) {
+            double sum = 0;
+            for (int j = i + 1; j < n; j++) {
+                sum = approximation.sigFig(sum + arr2[i][j] * ans[j], pres);
+            }
+            if (arr2[i][i] == 0) {
+                valid = false;
+                return;
+            }
+            ans[i] = approximation.sigFig(y[i] - sum, pres);
+        }
+    }
+
+    public double[] solve() {
+        ans = new double[n];
+        y = new double[n];
+        time = System.nanoTime();
+        forElimination();
+        forSubstitution();
+        DiagonalEvaluation();
+        backSubstitution();
+        time = System.nanoTime() - time;
+        return ans;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public boolean getValid() {
+        return valid;
+    }
+
+}
