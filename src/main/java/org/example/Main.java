7b770c6926251ac59aa855c79844b222ce242aa1
abdelrahman mohamed mohamed abdelmotlb <91913879+abdelmotlb@users.noreply.github.com>
2022-12-24 23:34:45 +0200

    update project with dependcences

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import org.example.GUI.GlobalFrame;
+
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) {
+        GlobalFrame fr = new GlobalFrame();
+    }
+}
